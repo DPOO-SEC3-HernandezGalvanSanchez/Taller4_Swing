f9ea9529388ec4b6333234896a3721077eea4f44
Daniel Hernandez Pineda <d.hernandez24@uniandes.edu.co>
2022-04-19 12:53:01 -0500

    Lineas Triqui

--- src/uniandes/dpoo/triqui/interfaz/PanelBotones.java
+++ src/uniandes/dpoo/triqui/interfaz/PanelBotones.java
@@ -1,22 +1,51 @@
 package uniandes.dpoo.triqui.interfaz;
 
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class PanelBotones extends JPanel implements ActionListener
 {
+	private VentanaTriqui padre;
+	private JLabel cantidadJugadas;
+	
+	private final String REINICIAR = "reiniciar";
+	
+	
 	public PanelBotones(VentanaTriqui padre)
 	{
+		this.padre = padre;
+		setLayout(new FlowLayout());
+		
+		JButton botonReiniciar = new JButton("Reiniciar");
+		botonReiniciar.addActionListener(this);
+		botonReiniciar.setActionCommand(REINICIAR);
+		add(botonReiniciar);
+		
+		cantidadJugadas = new JLabel("0 jugadas");
+		add(cantidadJugadas);
+		
 	}
 
 	public void actionPerformed(ActionEvent e)
 	{
+		String comando = e.getActionCommand();
+		
+		if (comando.equals(REINICIAR))
+		{
+			padre.reiniciar();
+		}
+
 	}
 
 	public void actualizarCantidadJugadas(int numero)
 	{
+		String mensaje = Integer.toString(numero) + " jugadas";
+		cantidadJugadas.setText(mensaje);		
 	}
 }
--- src/uniandes/dpoo/triqui/interfaz/PanelTriqui.java
+++ src/uniandes/dpoo/triqui/interfaz/PanelTriqui.java
@@ -1,5 +1,7 @@
 package uniandes.dpoo.triqui.interfaz;
 
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
@@ -12,9 +14,18 @@ import uniandes.dpoo.triqui.mundo.Triqui;
 @SuppressWarnings("serial")
 public class PanelTriqui extends JPanel implements MouseListener
 {
-
+	private VentanaTriqui padre;	
+	
+	private int X_origin;
+	private int Y_origin;
+	private int triquiLength;
+	
+	
 	public PanelTriqui(VentanaTriqui ventanaTriqui)
 	{
+		this.padre = ventanaTriqui;
+		setLayout(new BorderLayout());
+	
 	}
 
 	public void actualizarTablero(Triqui triqui)
@@ -25,6 +36,23 @@ public class PanelTriqui extends JPanel implements MouseListener
 	@Override
 	public void paint(Graphics g)
 	{
+		int width = this.getWidth();
+		int height = this.getHeight();
+		
+		if (width>height)
+		{
+			this.triquiLength = (int) Math.round(0.8*height);
+			this.X_origin = (int) Math.round((width/2) - (triquiLength/2));
+			this.Y_origin = (int) Math.round(0.1*height);
+		}
+		
+		else if (width<height)
+		{
+			this.Y_origin = (int) Math.round((height/2) - (triquiLength/2));
+			this.X_origin = (int) Math.round(0.1*width);
+			this.triquiLength = (int) Math.round(0.8*width);
+		}
+		
 		Graphics2D g2d = (Graphics2D) g;
 
 		pintarLineas(g2d);
@@ -38,6 +66,15 @@ public class PanelTriqui extends JPanel implements MouseListener
 
 	private void pintarLineas(Graphics2D g2d)
 	{
+		g2d.setStroke(new BasicStroke(5));
+
+		//Verticales
+		g2d.drawLine(X_origin+(triquiLength/3), Y_origin, X_origin+(triquiLength/3), Y_origin+triquiLength);
+		g2d.drawLine(X_origin+(2*triquiLength/3), Y_origin, X_origin+(2*triquiLength/3), Y_origin+triquiLength);
+		
+		//Horizontales
+		g2d.drawLine(X_origin, Y_origin+(triquiLength/3), X_origin+triquiLength, Y_origin+(triquiLength/3));
+		g2d.drawLine(X_origin, Y_origin+(2*triquiLength/3), X_origin+triquiLength, Y_origin+(2*triquiLength/3));
 	}
 
 	@Override
